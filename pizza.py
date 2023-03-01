6e6a3e536d230015534f969e409515db349d8c7e
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-01 16:10:12 -0600

    Updated pizza.py file

--- pizza.py
+++ pizza.py
@@ -5,8 +5,8 @@ class Pizza(Order):
     def __init__(self):
         super().__init__("Pizza")
 
-    def dish_name(self, string):
-        self.pizza = type
+    def dish_name(self, food):
+        self.pizza = food
 
     def price(self, int):
         self.pizza = (10)
