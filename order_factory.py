23c537ca49dab6790af57ad87c3b3fb7088b098d
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-01 15:34:32 -0600

    Updated order_factory.py file

--- order_factory.py
+++ order_factory.py
@@ -1,4 +1,13 @@
+from pizza import Pizza
+from pasta import Pasta
+from salad import Salad
+
 class OrderFactory():
 
-    def create_order(self):
-        pass
+    def create_order(self, type):
+        if type == "pizza":
+            return Pizza()
+        elif type == "pasta":
+            return Pasta()
+        elif type == "salad":
+            return Salad()
