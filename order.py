0ab4d2ec4cfbd38cdb9f4fd1289bcc30047990eb
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-01 15:05:15 -0600

    Updated order.py file

---
+++ order.py
@@ -0,0 +1,11 @@
+from pizza import Pizza
+from pasta import Pasta
+from salad import Salad
+
+class Order():
+
+    def dish_name(self, string):
+        pass
+
+    def price(self, int):
+        pass
