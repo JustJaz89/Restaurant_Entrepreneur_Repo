3f69666601e5b0da11cf9136dc0a0d7965f3059b
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-01 16:08:45 -0600

    Updated order.py file

--- order.py
+++ order.py
@@ -3,10 +3,10 @@ class Order():
     def __init__(self, type):
         self.type = type
 
-    def dish_name(self, string):
-        self.pizza = type
-        self.pasta = type
-        self.salad = type
+    def dish_name(self, food):
+        self.pizza = food
+        self.pasta = food
+        self.salad = food
 
     def price(self, int):
         self.pizza = (10)
