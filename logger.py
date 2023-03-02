30578db2cc8bb082de5f4799d931e69414c92241
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-02 13:15:51 -0600

    Updated logger.py file

--- logger.py
+++ logger.py
@@ -9,7 +9,7 @@ class Logger:
         self.salad_price = (8)
 
     def log_transaction(self, order, int):
-        self.accept_order_pizza = order
+        self.accept_order_pizza = self.pizza_price (10)
         self.accept_order_pasta = order
         self.accept_order_salad = order
         self.location_number = ()
