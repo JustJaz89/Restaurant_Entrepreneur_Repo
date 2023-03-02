48d7e8fb4089665261a0436b5f2f6389ae8c6ace
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-02 15:12:15 -0600

    Updated logger.py file

--- logger.py
+++ logger.py
@@ -1,15 +1,13 @@
 class Logger:
 
-    def transaction_count(self, int):
-        pass
-
-    def daily_sales(self, int):
-        self.pizza_price = (10)
-        self.pasta_price = (12)
-        self.salad_price = (8)
-
     def log_transaction(self, order, int):
         self.accept_order_pizza = self.pizza_price (10)
         self.accept_order_pasta = order
         self.accept_order_salad = order
         self.location_number = ()
+        pizza_price = (10)
+        pasta_price = (12)
+        salad_price = (8)
+
+        # transaction_count(int)
+        # daily_sales(int)
