c2bf29929918dfb8ed8fa0340ab4038759ac8216
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-02 15:29:48 -0600

    Updated franchise.py file

--- franchise.py
+++ franchise.py
@@ -1,11 +1,10 @@
 class Franchise:
 
-    def location_number(self, int):
-        pass
-
     def place_order(self):
         self.ask_for_order = (f"""What are you ordering?
         Type '1' for pizza, '2' for pasta, '3' for salad. """)
+
+# location_number(int)
         
 # OrderFactory.create_order()
 # logger.log_transaction()
