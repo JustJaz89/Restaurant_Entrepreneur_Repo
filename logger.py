830b1854d747dfff5aa62e614f5e01041c58efce
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-13 17:55:01 -0500

    Refactored Logger class

--- logger.py
+++ logger.py
@@ -1,13 +1,25 @@
+from pizza import Pizza
+
 class Logger:
+    def __init__(self):
+        self.transaction_count = 0
+        self.daily_sales = 0
 
-    def log_transaction(self, order, int):
-        self.accept_order_pizza = self.pizza_price (10)
-        self.accept_order_pasta = order
-        self.accept_order_salad = order
-        self.location_number = ()
-        pizza_price = (10)
-        pasta_price = (12)
-        salad_price = (8)
+    def log_transaction(self, customer_order, store_number):
+        self.transaction_count += 1
+        doc = open("log.txt", "a")
+        for item in customer_order:
+            self.daily_sales += item.price
 
-        # transaction_count(int)
-        # daily_sales(int)
+            doc.write(f"""\nPurchase at New Yorker Pizza store #{store_number}
+            Current transaction count: {self.transaction_count}
+            Dish Ordered: {item.dish_name}
+            Item Price: {item.price}
+            Combined Daily Income: {self.daily_sales}\n\n""")
+        doc.close()
+
+customer_order = [Pizza(), Pizza(), Pizza()]
+log = Logger()
+store_number = 777
+
+log.log_transaction(customer_order, store_number)
